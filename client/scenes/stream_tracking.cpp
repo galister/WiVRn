9abe71fea59c35b7570134e6545b9c577d9fd234
galister <22305755+galister@users.noreply.github.com>
2024-08-31 05:28:01 +0900

    experimental: steamvr-lh support

--- client/scenes/stream_tracking.cpp
+++ client/scenes/stream_tracking.cpp
@@ -20,6 +20,7 @@
 #include "application.h"
 #include "stream.h"
 #include "utils/ranges.h"
+#include "wivrn_packets.h"
 #include <spdlog/spdlog.h>
 #include <thread>
 
@@ -160,7 +161,7 @@ void scenes::stream::tracking()
 	auto get_int_extra = jni::klass("android/content/Intent").method<jni::Int>("getIntExtra", level_jstr, default_jint);
 
 	XrTime next_battery_check = 0;
-	const XrDuration battery_check_interval = 5'000'000'000; // 5s
+	const XrDuration battery_check_interval = 60'000'000'000; // 60s
 #endif
 	std::vector<std::pair<device_id, XrSpace>> spaces = {
 	        {device_id::HEAD, application::view()},
@@ -219,13 +220,16 @@ void scenes::stream::tracking()
 						j.fov = i.fov;
 					}
 
-					packet.flags = flags;
+					packet.view_flags = flags;
+
+					packet.state_flags = 0;
+					if (local_dirty.exchange(false))
+						packet.state_flags = xrt::drivers::wivrn::from_headset::tracking::recentered;
 
 					packet.device_poses.clear();
-					std::lock_guard lock(local_floor_mutex);
 					for (auto [device, space]: spaces)
 					{
-						packet.device_poses.push_back(locate_space(device, space, local_floor, t0 + Δt));
+						packet.device_poses.push_back(locate_space(device, space, stage, t0 + Δt));
 					}
 
 					t.pause();
@@ -235,13 +239,13 @@ void scenes::stream::tracking()
 					if (application::get_hand_tracking_supported())
 					{
 						hands.hand = xrt::drivers::wivrn::from_headset::hand_tracking::left;
-						hands.joints = locate_hands(application::get_left_hand(), local_floor, hands.timestamp);
+						hands.joints = locate_hands(application::get_left_hand(), stage, hands.timestamp);
 						t.pause();
 						network_session->send_stream(hands);
 						t.resume();
 
 						hands.hand = xrt::drivers::wivrn::from_headset::hand_tracking::right;
-						hands.joints = locate_hands(application::get_right_hand(), local_floor, hands.timestamp);
+						hands.joints = locate_hands(application::get_right_hand(), stage, hands.timestamp);
 						t.pause();
 						network_session->send_stream(hands);
 						t.resume();
